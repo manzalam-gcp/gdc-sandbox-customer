9b515c726da7f64b05ad1074586ff456106725c5
Brett Tackaberry <tackaberry@google.com>
2025-09-10 17:27:18 +0000

    always pull

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ fi
 
 if [ "$1" == "ollama" ]; then
     export IMAGE_NAME="ollama"
-    docker build -t $IMAGE_NAME ./ollama/
+    docker build -t $IMAGE_NAME ./open-web-ui/ollama/
     docker tag $IMAGE_NAME:latest $HARBOR_URL/$HARBOR_PROJECT/$IMAGE_NAME:latest
     docker push $HARBOR_URL/$HARBOR_PROJECT/$IMAGE_NAME:latest
 fi
