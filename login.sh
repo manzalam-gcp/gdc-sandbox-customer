89e86bba48b529f498ee50e66f87b000ef6d303d
Brett Tackaberry <tackaberry@google.com>
2025-09-16 17:13:43 +0000

    fix kube

--- login.sh
+++ login.sh
@@ -3,5 +3,5 @@
 source .env
 
 gdcloud auth login --login-config-cert=$HOME/org-1-web-tls-ca.cert
-gdcloud clusters get-credentials ${CLUSTER_NAME} --zone zone1 --kubeconfig=${KUBECONFIG}
+gdcloud clusters get-credentials ${CLUSTER_NAME} --zone zone1
 echo $HARBOR_PASSWORD | docker login $HARBOR_URL_HTTPS -u $HARBOR_USERNAME --password-stdin
