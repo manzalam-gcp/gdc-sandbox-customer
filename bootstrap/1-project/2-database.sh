904addebb73c8780c7780f3a4d74f94ec4896d37
Brett Tackaberry <tackaberry@google.com>
2025-09-22 17:58:54 +0000

    db name

--- bootstrap/1-project/2-database.sh
+++ bootstrap/1-project/2-database.sh
@@ -2,7 +2,7 @@
 
 source .env
 
-gdcloud database clusters create $DB_CLUSTER_NAME --database-version $DB_VERSION --admin-password $DB_ADMIN_PASSWORD --project $WORKLOAD_PROJECT
+gdcloud database clusters create $DB_ALLOY_CLUSTER_NAME --database-version $DB_VERSION --admin-password $DB_ADMIN_PASSWORD --project $WORKLOAD_PROJECT
 
-gdcloud database clusters create db-psql-tack-1 --database-version POSTGRESQL_13 --admin-password $DB_ADMIN_PASSWORD --project $WORKLOAD_PROJECT
+gdcloud database clusters create $DB_PSQL_CLUSTER_NAME --database-version POSTGRESQL_13 --admin-password $DB_ADMIN_PASSWORD --project $WORKLOAD_PROJECT
 
