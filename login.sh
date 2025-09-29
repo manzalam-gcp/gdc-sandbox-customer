7aba10361f1b82f6b45f3b0a1aae4807a244a21a
Brett Tackaberry <tackaberry@google.com>
2025-09-29 20:36:26 +0000

    helpers

--- login.sh
+++ login.sh
@@ -3,5 +3,9 @@
 source .env
 
 gdcloud auth login --login-config-cert=$HOME/org-1-web-tls-ca.cert
-gdcloud clusters get-credentials ${CLUSTER_NAME} --zone zone1
+
+KUBECONFIG=${HOME}/${CLUSTER_NAME}-kubeconfig gdcloud clusters get-credentials ${CLUSTER_NAME} --zone zone1
+KUBECONFIG=${HOME}/org-1-admin-kubeconfig gdcloud clusters get-credentials org-1-admin
+KUBECONFIG=${HOME}/global-api-kubeconfig gdcloud clusters get-credentials global-api
+
 echo $HARBOR_PASSWORD | docker login $HARBOR_URL_HTTPS -u $HARBOR_USERNAME --password-stdin
